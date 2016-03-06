c38cc3227648b379981f9a0e6ed9d7fcfb4ac045
Matthew Salim <matthew.n.salim@gmail.com>
2016-03-06 07:49:32 -0500

    Attempt on delete

--- Plan/Plan/RectanglePrinter.py
+++ Plan/Plan/RectanglePrinter.py
@@ -3,5 +3,6 @@ for y in range(2, 16, 2):
 	for x in range(0, 96, 2):
 		#print(("<Rectangle x:Name=\"r{n}\" Grid.Row=\"{a}\" Grid.Column=\"{b}\" Fill=\"Transparent\" Margin=\"0\" Stroke=\"Transparent\" Tapped=\"Rectangle_Tapped\" Tag=\"r{n}\"/>".format(n=z, a=x, b=y)))
 		#print(("<TextBlock x:Name=\"t{n}\" Grid.Row=\"{a}\" Grid.Column=\"{b}\" TextWrapping=\"Wrap\" Text=\"\" Grid.RowSpan=\"10\"/>".format(n=z, a=x, b=y)))
-		print("else if(i={n}) t{n}.Text = eventName;".format(n=z));
+		#print("else if(first == {n}) t{n}.Text = eventName;".format(n=z));
+		print("if(first <= {n} && {n} <= last) r{n}.Fill = col;".format(n=z));
 		z = z+1;
